e41b9255dde618515888ff7ed3e8c83d636f2fac
nickmwangemi <nickmwangemi@gmail.com>
2022-02-18 00:32:14 +0300

    Add script to load csv data into db

--- records/admin.py
+++ records/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import AdGroup, Campaign, SearchTerm
 
 # Register your models here.
+class AdGroupAdmin(admin.ModelAdmin):
+    pass
+
+class CampaignAdmin(admin.ModelAdmin):
+    pass
+
+class SearchTermAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(AdGroup, AdGroupAdmin)
+admin.site.register(Campaign, CampaignAdmin)
+admin.site.register(SearchTerm, SearchTermAdmin)
--- records/models.py
+++ records/models.py
@@ -1,3 +1,58 @@
 from django.db import models
 
-# Create your models here.
+"""
+adgroups
+- ad_group_id
+- campaign_id
+- alias
+- status
+"""
+class AdGroup(models.Model):
+    ad_group_id = models.BigIntegerField()
+    campaign_id = models.BigIntegerField()
+    alias = models.CharField(max_length=255)
+    status = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"AdGroup object with {self.ad_group_id} has been created."
+
+
+"""
+campaigns
+- campaign_id
+- structure_value
+- status
+"""
+class Campaign(models.Model):
+    campaign_id = models.BigIntegerField()
+    structure_value = models.CharField(max_length=255)
+    status = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"Campaign object with {self.campaign_id} has been created."
+
+
+
+"""
+search_terms
+- date
+- ad_group_id
+- campaign_id
+- clicks
+- cost
+- conversion_value
+- conversions
+- search_term
+"""
+class SearchTerm(models.Model):
+    date = models.DateField()
+    ad_group_id = models.BigIntegerField()
+    campaign_id = models.BigIntegerField()
+    clicks = models.BigIntegerField()
+    cost = models.DecimalField(max_digits=5, decimal_places=2)
+    conversion_value =  models.DecimalField(max_digits=5, decimal_places=2)
+    conversions = models.BigIntegerField()
+    search_term = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"SearchTerm object with {self.campaign_id} has been created."
